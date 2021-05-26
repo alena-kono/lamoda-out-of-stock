59b3a0298e872759bf1e6eab23eeac28ec77b2f8
Alena Konovalova <konovalova.alena@gmail.com>
2021-05-26 22:00:49 +0300

    Add opportunity to set up lamoda environment

--- app/config.py
+++ app/config.py
@@ -7,9 +7,14 @@ dotenv.load_dotenv()
 # flask config
 SECRET_KEY = os.environ.get('FLASK_SECRET_KEY')
 
-# lamoda credentials
-CLIENT_ID = os.environ.get('CLIENT_ID')
-CLIENT_SECRET = os.environ.get('CLIENT_SECRET')
+# lamoda environment config
+LAMODA_ENV_PRODUCTION = True
 
-# lamoda api environment
-LAMODA_ENV_URL = 'https://api-demo-b2b.lamoda.ru'
+if LAMODA_ENV_PRODUCTION:
+    CLIENT_ID = os.environ.get('CLIENT_ID_PRODUCTION')
+    CLIENT_SECRET = os.environ.get('CLIENT_SECRET_PRODUCTION')
+    LAMODA_ENV_URL = 'https://api-b2b.lamoda.ru'
+else:
+    CLIENT_ID = os.environ.get('CLIENT_ID_DEMO')
+    CLIENT_SECRET = os.environ.get('CLIENT_SECRET_DEMO')
+    LAMODA_ENV_URL = 'https://api-demo-b2b.lamoda.ru'
