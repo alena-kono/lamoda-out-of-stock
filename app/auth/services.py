28f3b1303820ce8ca067d60cf1a7e386e8c38b00
Alena Konovalova <konovalova.alena@gmail.com>
2021-05-27 23:13:42 +0300

    Fix typing

--- app/auth/services.py
+++ app/auth/services.py
@@ -4,7 +4,7 @@ from app.auth.auth import Auth
 from app.config import CLIENT_ID, CLIENT_SECRET, LAMODA_ENV_URL
 
 
-def get_auth_headers() -> Dict[str]:
+def get_auth_headers() -> Dict[str, str]:
     url = LAMODA_ENV_URL
     a = Auth(url, CLIENT_ID, CLIENT_SECRET)
     return a.get_oauth2_headers()
