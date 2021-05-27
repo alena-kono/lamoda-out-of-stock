9b83794817903c2a1723e39837e499ec6a854b4c
Alena Konovalova <konovalova.alena@gmail.com>
2021-05-27 23:12:07 +0300

    Refactor auth package

---
+++ app/auth/auth.py
@@ -0,0 +1,44 @@
+from typing import Dict, NoReturn, Optional
+
+import requests
+from app.auth.exceptions import AccessTokenError
+
+
+class Auth:
+    def __init__(
+        self, domain_url: str, client_id: str, client_secret: str
+            ) -> None:
+        self.domain_url = domain_url
+        self.client_id = client_id
+        self.client_secret = client_secret
+        self.access_token = ''
+
+    def _get_request_params(self) -> Dict[str, str]:
+        return {
+            'client_id': self.client_id,
+            'client_secret': self.client_secret,
+            'grant_type': 'client_credentials',
+            }
+
+    def _get_auth_response(self) -> requests.Response:
+        request_url = ''.join((self.domain_url, '/auth/token'))
+        params = self._get_request_params()
+        return requests.get(request_url, params=params)
+
+    def _get_access_token(response: requests.Response) -> Optional[str]:
+        if response.status_code == 200:
+            return str(response.json().get('access_token'))
+        return None
+
+    def _set_access_token(self) -> Optional[NoReturn]:
+        if access_token := self._get_access_token():
+            self.access_token = access_token
+            return None
+        raise AccessTokenError('Access token is missing')
+
+    def get_oauth2_headers(self) -> Dict[str, str]:
+        self._set_access_token()
+        return {
+            'Content-type': 'application/json',
+            'Authorization': f'Bearer {self.access_token}'
+            }
--- app/auth/services.py
+++ app/auth/services.py
@@ -1,50 +1,10 @@
-from typing import Dict, NoReturn, Optional
+from typing import Dict
 
-import requests
-from app.auth.exceptions import AccessTokenError
-
+from app.auth.auth import Auth
 from app.config import CLIENT_ID, CLIENT_SECRET, LAMODA_ENV_URL
 
 
-class Auth:
-    def __init__(
-        self, domain_url: str, client_id: str, client_secret: str
-            ) -> None:
-        self.domain_url = domain_url
-        self.client_id = client_id
-        self.client_secret = client_secret
-        self.access_token = ''
-
-    def _get_request_params(self) -> Dict[str, str]:
-        return {
-            'client_id': self.client_id,
-            'client_secret': self.client_secret,
-            'grant_type': 'client_credentials',
-            }
-
-    def _get_access_token(self) -> Optional[str]:
-        request_url = ''.join((self.domain_url, '/auth/token'))
-        params = self._get_request_params()
-        response = requests.get(request_url, params=params)
-        if response.status_code == 200:
-            return str(response.json().get('access_token'))
-        return None
-
-    def _set_access_token(self) -> Optional[NoReturn]:
-        if access_token := self._get_access_token():
-            self.access_token = access_token
-            return None
-        raise AccessTokenError('Access token is missing')
-
-    def get_oauth2_headers(self) -> Dict[str, str]:
-        self._set_access_token()
-        return {
-            'Content-type': 'application/json',
-            'Authorization': f'Bearer {self.access_token}'
-            }
-
-
-def get_auth_headers():
+def get_auth_headers() -> Dict[str]:
     url = LAMODA_ENV_URL
     a = Auth(url, CLIENT_ID, CLIENT_SECRET)
     return a.get_oauth2_headers()
