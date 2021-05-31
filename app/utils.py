3a1534df5e2e57abb1f21b4aa96f9a46353e7dcc
Alena Konovalova <konovalova.alena@gmail.com>
2021-05-31 22:38:25 +0300

    Add checks and exception handling to utils

--- app/utils.py
+++ app/utils.py
@@ -1,9 +1,14 @@
+from json.decoder import JSONDecodeError
 from typing import Dict, List, Optional
 
 import requests
 
+from app.exceptions import NoJsonError
+
 
 def create_url(urls: List[str], params: Optional[Dict] = None) -> str:
+    if not urls:
+        raise ValueError('urls should not be empty list')
     urls_str = ''.join(urls)
     if params:
         if not isinstance(params, dict):
@@ -16,4 +21,13 @@ def create_url(urls: List[str], params: Optional[Dict] = None) -> str:
 
 
 def parse_json_response(response: requests.Response) -> dict:
-    return response.json()
+    if not isinstance(response, requests.Response):
+        raise TypeError(
+            'response should be <class \'requests.Response\',',
+            f'not {type(response)}'
+            )
+    try:
+        parsed_json = response.json()
+    except JSONDecodeError:
+        raise NoJsonError()
+    return parsed_json
