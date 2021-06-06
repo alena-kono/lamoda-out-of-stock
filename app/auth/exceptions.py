7d2b3dd364ad9de8a64ad903c9f1f9adca4ff9f6
Alena Konovalova <konovalova.alena@gmail.com>
2021-06-06 22:14:56 +0300

    Add checks for client credentials

--- app/auth/exceptions.py
+++ app/auth/exceptions.py
@@ -4,3 +4,8 @@ from app.exceptions import BaseAppException
 class AccessTokenError(BaseAppException):
     '''Access token is invalid or missing'''
     pass
+
+
+class ClientCredentialsError(BaseAppException):
+    '''Client credentials are missing'''
+    pass
--- app/auth/services.py
+++ app/auth/services.py
@@ -1,10 +1,13 @@
 from typing import Dict
 
 from app.auth.auth import Auth
+from app.auth.exceptions import ClientCredentialsError
 from app.config import CLIENT_ID, CLIENT_SECRET, LAMODA_ENV_URL
 
 
 def get_auth_headers() -> Dict[str, str]:
     url = LAMODA_ENV_URL
-    a = Auth(url, CLIENT_ID, CLIENT_SECRET)
-    return a.get_oauth2_headers()
+    if CLIENT_ID and CLIENT_SECRET:
+        a = Auth(url, CLIENT_ID, CLIENT_SECRET)
+        return a.get_oauth2_headers()
+    raise ClientCredentialsError('Client credentials are missing')
