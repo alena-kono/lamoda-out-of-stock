a737a03788aa3391210b49ba6585a4925b43e4ed
Alena Konovalova <konovalova.alena@gmail.com>
2021-06-06 22:20:13 +0300

    Remove extra variable

--- app/auth/services.py
+++ app/auth/services.py
@@ -6,8 +6,7 @@ from app.config import CLIENT_ID, CLIENT_SECRET, LAMODA_ENV_URL
 
 
 def get_auth_headers() -> Dict[str, str]:
-    url = LAMODA_ENV_URL
     if CLIENT_ID and CLIENT_SECRET:
-        a = Auth(url, CLIENT_ID, CLIENT_SECRET)
+        a = Auth(LAMODA_ENV_URL, CLIENT_ID, CLIENT_SECRET)
         return a.get_oauth2_headers()
     raise ClientCredentialsError('Client credentials are missing')
