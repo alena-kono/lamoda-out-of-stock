8d4c2b3e5e6f5a25810a6043a334f4e618ef1741
Alena Konovalova <konovalova.alena@gmail.com>
2021-06-08 00:15:51 +0300

    Add type hinting

--- app/exceptions.py
+++ app/exceptions.py
@@ -9,6 +9,6 @@ class NoJsonError(BaseAppException):
     '''The exception is raised when Response object does not
     contain a valid json.
     '''
-    def __init__(self):
+    def __init__(self) -> None:
         message = 'Response object does not contain a valid json'
         super().__init__(message)
