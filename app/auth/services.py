a5d49f3e7aa35f392740add986fd22b13872f9e7
Alena Konovalova <konovalova.alena@gmail.com>
2021-06-13 00:47:10 +0300

    Tailor to new config

--- app/auth/services.py
+++ app/auth/services.py
@@ -2,11 +2,12 @@ from typing import Dict
 
 from app.auth.auth import Auth
 from app.auth.exceptions import ClientCredentialsError
-from app.config import CLIENT_ID, CLIENT_SECRET, LAMODA_ENV_URL
+from app.config import conf
 
 
 def get_auth_headers() -> Dict[str, str]:
-    if CLIENT_ID and CLIENT_SECRET:
-        a = Auth(LAMODA_ENV_URL, CLIENT_ID, CLIENT_SECRET)
+    credentials = conf.get_config()[0:3]
+    if all(credentials):
+        a = Auth(*credentials)
         return a.get_oauth2_headers()
     raise ClientCredentialsError('Client credentials are missing')
