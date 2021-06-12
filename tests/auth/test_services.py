b23487f0fd26b27d0d67fa9c73c3f2f1802d7654
Alena Konovalova <konovalova.alena@gmail.com>
2021-06-13 00:49:00 +0300

    Add tests

--- tests/auth/test_services.py
+++ tests/auth/test_services.py
@@ -1,10 +1,6 @@
-import requests
 from app.auth.services import get_auth_headers
 
 
-def test_get_auth_headers(sample_credentials, request_mocker, expected_oauth2_headers):
-    LAMODA_ENV_URL, CLIENT_ID, CLIENT_SECRET = sample_credentials
-    request_mocker.get(LAMODA_ENV_URL)
-    requests.get(LAMODA_ENV_URL)
+def test_get_auth_headers(expected_oauth2_headers, sample_auth_response_200):
     headers = get_auth_headers()
     assert headers == expected_oauth2_headers
