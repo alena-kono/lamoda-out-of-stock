b34f63956795a1405df367e48ed9d96fed96fd3e
Alena Konovalova <konovalova.alena@gmail.com>
2021-06-14 16:51:47 +0300

    Add test

--- tests/auth/test_services.py
+++ tests/auth/test_services.py
@@ -1,6 +1,16 @@
+import pytest
+from app.auth.exceptions import ClientCredentialsError
 from app.auth.services import get_auth_headers
 
 
 def test_get_auth_headers(expected_oauth2_headers, sample_auth_response_200):
     headers = get_auth_headers()
     assert headers == expected_oauth2_headers
+
+
+def test_get_auth_headers_with_missing_credentials(
+    sample_auth_response_200,
+    missing_env_vars
+        ):
+    with pytest.raises(ClientCredentialsError):
+        get_auth_headers()
