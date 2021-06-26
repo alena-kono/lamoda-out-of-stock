28e55defa3744230050828ccd0e5d66064d5a621
Alena Konovalova <konovalova.alena@gmail.com>
2021-06-26 19:21:28 +0300

    Refactor common app exceptions

--- app/exceptions.py
+++ app/exceptions.py
@@ -1,29 +1,31 @@
 class BaseAppException(Exception):
-    '''Generic exception for this application.
+    ''':exception: generic exception for this application.
     All custom exceptions should be inherited from this one.
     '''
     pass
 
 
 class NoJsonError(BaseAppException):
-    '''Raised when Response object does not
+    ''':exception: raised when Response object does not
     contain a valid json.
     '''
-    def __init__(self) -> None:
-        message = 'Response object does not contain a valid json'
-        super().__init__(message)
+    def __init__(self, msg: str = '') -> None:
+        if not msg:
+            msg = 'Response object contains invalid json'
+        super().__init__(msg)
 
 
 class InvalidConfigError(BaseAppException):
-    '''Raised when invalid config name is passed'''
-    def __init__(self) -> None:
-        message = 'Invalid config name is passed'
-        super().__init__(message)
+    ''':exception: raised when invalid config name is passed'''
+    def __init__(self, msg: str = '') -> None:
+        if not msg:
+            msg = 'Invalid config name is passed'
+        super().__init__(msg)
 
 
 class ConvertionError(BaseAppException):
-    '''Raised when str cannot be converted to int'''
-    def __init__(self, message: str) -> None:
-        if not message:
-            message = 'str cannot be converted to int'
-        super().__init__(message)
+    ''':exception: raised when str cannot be converted to int'''
+    def __init__(self, msg: str = '') -> None:
+        if not msg:
+            msg = 'Str cannot be converted to int'
+        super().__init__(msg)
